68b8f506a4edc3ca9f06bb0a524c45aaf1675c20
Kevin Schroeder <kschroeder@mirageworks.com>
2017-04-19 17:38:18 -0500

    Updated instructions to enable the integration

--- examples/server/index.php
+++ examples/server/index.php
@@ -8,7 +8,8 @@ $config = [
     'magium' => [
         'ad' => [
             'client_id' => '',
-            'client_secret' => ''
+            'client_secret' => '',
+            'enabled' => 'yes'
         ]
     ]
 ];
